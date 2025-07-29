07a45988a74fe84f385c517ab4864734354eb8d7
pranitamane25 <pranitamane2506@gmail.com>
2025-07-29 14:57:02 +0530

    database connectivity using dotnet

---
+++ simpledb/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Connection string to your MySQL database
+        string connectionString = "Server=localhost; Port=3306; Database=emp; User=root; Password=password";
+
+        using (MySqlConnection con = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                con.Open();
+                Console.WriteLine("Connected to MySQL!");
+
+                string query = "SELECT * FROM customers";
+                using (MySqlCommand cmd = new MySqlCommand(query, con))
+                {
+                    cmd.CommandType = CommandType.Text;
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+            Console.WriteLine($"customer_id: {reader["customer_id"]}, name: {reader["name"]}, email: {reader["email"]}, signup_date: {reader["signup_date"]}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
