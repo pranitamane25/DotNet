c929e1d6f98e35a94628ab0258eeea7443dd40d8
pranitamane25 <pranitamane2506@gmail.com>
2026-06-01 12:00:03 +0530

    basic program of cs

---
+++ Basics/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Hello{
+    public static void Main(string[] args){
+
+        calculate();
+        sum();
+    }
+        public static void calculate(){
+            Console.WriteLine("Hello World");
+        }
+
+        public static void sum()
+    {
+        Console.WriteLine("Pranita");
+        Console.ReadKey();
+    }
+    }
